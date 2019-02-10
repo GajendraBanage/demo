6adb3b4bd5806444d9d8bd7a0d5e6ed5af104a56
gajendrabanage <gajendrabanage2312@gmail.com>
2019-02-10 19:57:40 +0530

    fihdfziouh

--- Hibernate/java/com/model/App.java
+++ Hibernate/java/com/model/App.java
@@ -6,8 +6,9 @@ package com.model;
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!  wsewer dfrw adas" );
     }
 }
