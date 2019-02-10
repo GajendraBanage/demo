13ca11336eb8e76b26b554d7e3c9acca47435afc
gajendrabanage <gajendrabanage2312@gmail.com>
2019-02-10 20:02:48 +0530

    Merge branch 'master' of https://github.com/GajendraBanage/demo
    # Conflicts:
    #	Hibernate/java/com/model/App.java

--- Hibernate/java/com/model/App.java
+++ Hibernate/java/com/model/App.java
@@ -9,6 +9,6 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!  wsewer dfrw adas" );
+        System.out.println( "Hello World!  wse  wer dfrw adas" );
     }
 }
